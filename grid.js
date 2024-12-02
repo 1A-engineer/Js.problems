af125b8c759e8c907355e1bb3f0e8965fddd82e6
Second <second@Bat-Erdenes.local>
2024-12-02 18:49:15 +0800

    homework

--- grid.js
+++ grid.js
@@ -70,7 +70,9 @@ let minutes10 = x10 % 60
 let seconds10 = x10 % 3600
 console.log (hours10,  minutes10, seconds10)
 
-//11 
+
+
+//11 (largest one amoung three numbers )
 const num1 = (14);
 const num2 = (55);
 const num3 = (64);
@@ -86,3 +88,82 @@ else {
     largest = num3;
 }
 console.log(largest)
+
+// 12 (smallest one in three numbers)
+
+const num11 = 45;
+const num12 = 40;
+const num13 = 78;
+let smallest; 
+if (num11<=num12 && num11<=num13) {
+  smallest = num11;
+}
+
+else if (num12<=num11 && num12<=num13)
+{
+  smallest = num12;
+}
+
+else 
+  {
+    smallest = num13;
+  }
+
+  console.log(smallest)
+
+  // 13 (Sum of numbers more than 80)
+
+const numbers = [50, 90, 30, 85, 60, 78, 100, 20];
+let sum = 0;
+for (let num of numbers) {
+  if (num > 80) {
+    sum += num; 
+  }
+}
+console.log("Sum of numbers more than 80:", sum);
+
+// 14 (Multiplication of numbers lower than 5)
+
+const givenNumbers = [3, 6, 2, 4];
+let totalSum = 1;
+for (let currentNumber of givenNumbers) {
+  if (currentNumber < 5) {
+    totalSum *= currentNumber;
+  }
+}
+console.log("Sum of numbers lower than 5:", totalSum);
+
+
+// 15 (Sum of even numbers)
+
+function isEven(numbers15) {
+  return numbers15 % 2 === 0;
+}
+
+const problem15 = [1, 2, 3, 4, 5, 6];
+let sumOfEvens = 0;
+
+for (let num15 of problem15) {
+  if (isEven(num15)) {
+    sumOfEvens += num15;
+  }
+}
+console.log("Sum of even numbers:", sumOfEvens);
+
+// 16 (Multiplication of odd numbers)
+
+
+function multiplyOddNumbers(arr) {
+  let product = 1;
+  arr.forEach((num) => {
+    if (num % 2 !== 0) {
+      product *= num;
+    }
+  });
+  return product;
+}
+let numbers16 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(multiplyOddNumbers(numbers16));
+
+
+
