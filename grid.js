c7dd98d49da268410dc7a2ea13fe9b2fedb55f65
1A-engineer <drewuncle863724lu@gmail.com>
2024-12-11 11:59:30 +0800

    11st Dex

--- grid.js
+++ grid.js
@@ -744,5 +744,26 @@ console.log(transformEmail('bat.a@gmail.com'));
 console.log(transformEmail('bat.kh@gmail.com'));  
 
 
+// Binary search
+
+function searchInsert(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    
+    if (nums[mid] === target) {
+      return mid;  
+    }
+    if (nums[mid] < target) {
+      left = mid + 1;  
+    } else {
+      right = mid - 1; 
+    }
+  }
+  
+  return left;  
+}
 
 
